7f938b50669789a780827bc123b4913cf7efd925
danielwiehl <daniel.wiehl@yahoo.com>
2022-06-06 17:18:35 +0200

    feat(customers-app): open customer in the `aside` outlet

--- apps/customers-app/src/app/customer-list/customer-list.component.ts
+++ apps/customers-app/src/app/customer-list/customer-list.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import {Router} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {Customer, CustomerService} from '../customer.service';
+import {OutletRouter} from '@scion/microfrontend-platform';
 
 @Component({
   selector: 'app-customer-list',
@@ -14,11 +14,11 @@ export class CustomerListComponent {
 
   public customers: Customer[];
 
-  constructor(customerService: CustomerService, private router: Router) {
+  constructor(customerService: CustomerService, private router: OutletRouter) {
     this.customers = customerService.getCustomers();
   }
 
   public onOpenCustomer(id: string): void {
-    this.router.navigate([`/customers/${id}`]);
+    this.router.navigate(`/customers/${id}`, {outlet: 'aside'});
   }
 }
