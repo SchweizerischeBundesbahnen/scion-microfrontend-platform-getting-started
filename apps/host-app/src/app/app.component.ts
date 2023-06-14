537034a7cd86bfa407e45860a3de296a357c41cb
Marcarrian <marc.muster@bluewin.ch>
2023-06-14 14:13:31 +0200

    feat(host-app): bootstrap application using standalone component

--- apps/host-app/src/app/app.component.ts
+++ apps/host-app/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ManifestService, MicrofrontendCapability, OutletRouter, Qualifier} from '@scion/microfrontend-platform';
 import {filterArray} from '@scion/toolkit/operators';
 import {Observable} from 'rxjs';
+import {AsyncPipe, NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    NgFor,
+  ],
 })
 export class AppComponent {
 
