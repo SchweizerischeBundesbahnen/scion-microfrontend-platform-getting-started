ee730e606f0982a76dec4386fb2dbfa302039f7e
Marcarrian <marc.muster@bluewin.ch>
2023-06-14 15:20:22 +0200

    feat(host-app): bootstrap application using standalone component

--- apps/host-app/src/app/app.component.ts
+++ apps/host-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    RouterOutlet,
+  ],
 })
 export class AppComponent {
 }
