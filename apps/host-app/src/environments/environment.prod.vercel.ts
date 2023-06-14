6d4fdc795001f73c5e230f8dddc8bf2c42116b21
Marcarrian <marc.muster@bluewin.ch>
2023-06-14 10:59:36 +0200

    deps: update to Angular 15

--- apps/host-app/src/environments/environment.prod.vercel.ts
+++ apps/host-app/src/environments/environment.prod.vercel.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
   PRODUCTS_APP_MANIFEST_URL: 'https://scion-microfrontend-platform-getting-started-products-angular.vercel.app/assets/manifest.json',
   CUSTOMERS_APP_MANIFEST_URL: 'https://scion-microfrontend-platform-getting-started-customers-angular.vercel.app/assets/manifest.json',
   DEV_TOOLS_MANIFEST_URL: 'https://scion-microfrontend-platform-devtools.vercel.app/assets/manifest.json',
--- apps/host-app/src/environments/environment.ts
+++ apps/host-app/src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
   PRODUCTS_APP_MANIFEST_URL: 'http://localhost:4201/assets/manifest.json',
   CUSTOMERS_APP_MANIFEST_URL: 'http://localhost:4202/assets/manifest.json',
   DEV_TOOLS_MANIFEST_URL: 'https://scion-microfrontend-platform-devtools.vercel.app/assets/manifest.json',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
