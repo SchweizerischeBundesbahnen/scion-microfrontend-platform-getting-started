96b518f8ab08fb7a2667da69e4fc3e544767d5d8
danielwiehl <daniel.wiehl@yahoo.com>
2024-10-30 14:53:22 +0100

    chore: move getting started app to `*.scion.vercel.app` subdomain
    Previous URL: https://scion-microfrontend-platform-getting-started-angular.vercel.app
    New URL: https://microfrontend-platform-getting-started-angular.scion.vercel.app

--- apps/host-app/src/environments/environment.prod.ts
+++ apps/host-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   PRODUCTS_APP_MANIFEST_URL: 'http://localhost:4201/assets/manifest.json',
   CUSTOMERS_APP_MANIFEST_URL: 'http://localhost:4202/assets/manifest.json',
-  DEV_TOOLS_MANIFEST_URL: 'https://scion-microfrontend-platform-devtools.vercel.app/assets/manifest.json',
+  DEV_TOOLS_MANIFEST_URL: 'https://microfrontend-platform-devtools.scion.vercel.app/assets/manifest.json',
 };
